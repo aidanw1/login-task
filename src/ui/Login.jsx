8109bd402d99d52cbec33062aac42cff19760149
Aidan Walker <aidan@aidans-mbp.home>
2020-09-23 14:08:00 +0100

    first submit

---
+++ src/ui/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+export default function Login() {
+  const [user, setUser] = useState("");
+
+  return (
+    <div className="login-container">
+      <div className="login-container__content">
+        <img src="./green.svg" alt="green energy company logo" />
+        <div className="headers">
+          <h2 className="headers__heading">Example login screen</h2>
+          <h3 className="headers__sub-heading">Getting started with Green.</h3>
+        </div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setUser("");
+            alert(`${user} has logged in`);
+          }}
+        >
+          <div className="login-container__content__input-box">
+            <label htmlFor="email">Email Address</label>
+            <input
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+              className="input"
+              type="email"
+              id="email"
+              maxLength="30"
+            />
+          </div>
+          <div className="login-container__content__remember-device">
+            <input type="checkbox" />
+            <label>Remember this device</label>
+          </div>
+
+          <button
+            disabled={user.length < 1}
+            className="login-container__content--btn"
+            type="submit"
+            value="Submit"
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
